d0f18eeb65080802064c7ee60b38b40320bac53d
SonarBoy <Joshua.L99.C@gmail.com>
2019-06-03 18:35:00 -0400

    Basic Pattern outline

---
+++ Adapter-Pattern/MP4Player.java
@@ -0,0 +1,12 @@
+public class MP4Player implements AdvancedMediaPlayer{
+    
+    @Override
+    public void playVLC(String fileName){
+
+    }
+
+    @Override
+    public void playMP4(String fileName){
+        System.out.println("Playing MP4 file. Name: " + fileName);
+    }
+}
